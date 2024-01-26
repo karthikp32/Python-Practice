e17534095289bf040afe5dd2b8cfbc4721741882
Karthik Pillalamarri <karthik.pillalamarri.kp@gmail.com>
2024-01-26 18:22:16 +0000

    implemented queues, sets, map, heaps, tuples, functions, classes

--- main.py
+++ main.py
@@ -255,4 +255,245 @@ print(ord("a"))
 strings = ["karthik", "swathi", "abhi", "akhil"]
 print("/".join(strings))
 
+#Queues are doubled ended queues by default
+#you can use deques if you want to use a data structure to work like a stack
+#you can append to the right, pop from the right, append to the left ,pop from the left
+#O(1) time for these enqueuing and dequeuing operations I think
+
+from collections import deque 
+
+queue = deque()
+queue.append(3)
+queue.append(1)
+queue.append(4)
+print(queue)
+
+queue.popleft()
+print(queue)
+
+
+queue.pop()
+print(queue)
+
+queue.appendleft(2)
+print(queue)
+
+
+#You can use hashset to store unique values and search and insert a value in O(1) time
+tempSet = set()
+
+tempSet.add(1)
+tempSet.add(2)
+tempSet.add(3)
+
+
+print(tempSet)
+print(len(tempSet))
+
+#You can use "in" operator to check if an element exists in the set
+print(1 in tempSet)
+print(3 in tempSet)
+print("Karthik" in tempSet)
+
+
+#You can remove values in O(1) time
+tempSet.remove(3)
+print(3 in tempSet)
+
+
+#You can create a set from a list in python
+print(set([3,1,4]))
+
+# You can use set comprehension to initialize a set and insert values 
+# similar to how you would for a certain range values 0...n with a for loop
+
+# HashMap AKA Dictionaries in Python, Maps in Java, Objects in Javascript
+# can't have duplicate keys
+myMap = {}
+myMap["karthik"] = 32
+myMap["karthik"] = 23
+myMap["swathi"] = 17
+print(myMap)
+
+#Use len to get number of key value pairs
+print(len(myMap))
+
+#you can remove key value pair by using pop() of hashmap
+myMap.pop("karthik")
+print(len(myMap))
+
+
+#You can use "in" operator to check if an element exists as key in the map
+print("swathi" in myMap)
+
+
+#you can initialize maps with concrete values 
+myMap2 = {"color1": "blue", "color2": "red"}
+print(myMap2)
+
+
+# You can use dictionary comprehension to initalize a dictionary/map with 
+# with concrete key values pair for a range of values 0,1..n just like
+# you would with a for loop
+myMap3 = {i: i*i for i in range(10)}
+print(myMap3)
+
+
+#Looping through maps
+
+#You can loop through the keys
+for key in myMap3:
+    print(key)
+
+#You can loop through the values
+for val in myMap3.values():
+    print(val)
+
+#You can loop through keys and values using items() and unpacking
+for key, val in myMap3.items():
+    print(key, val)    
+
+
+#Tuples are like immutable arrays that you use () to initialize
+#you can't change the values in a tuple after you initialize it
+# with certain values
+
+pi = (3, 1, 4)
+print(pi)
+print(pi[2])
+
+#You can use as key in hashmap or element in hashset
+myMap4 = {("karthik", "pillalamarri"): "984 N. High St."}
+print(myMap4)
+
+mySet2 = set()
+mySet2.add(("karthik", "pillalamarri"))
+mySet2.add(("akhil", "pillalamari"))
+print(mySet2)
+print(("akhil", "pillalamari") in mySet2)
+
+#Lists can't be keys for hashmap since they can have duplicate values so you can't generate unique
+# hash values which makes them not hashable I think 
+# myMap5 = {[3,1,4]: "pi" }
+# print(myMap5)
+
+# Heaps
+import heapq
+
+# heaps are min heaps by default
+# can use heap to find the min/max values of a set of values in O(1) time
+
+# implemented using arrays under the hood
+# you can initialize using [] just like arrays
+minHeap = []
+heapq.heappush(minHeap, 8)
+heapq.heappush(minHeap, 2)
+heapq.heappush(minHeap, 10)
+
+#min will always be at index 0
+# print(minHeap[0])
+
+#you can keep pop values from minHeap while length is non-zero
+while len(minHeap):
+    print(heapq.heappop(minHeap))
+
+# to create max heap, create min heap and multiply each value by -1
+# when you push and pop
+# for ex. 1,2, 1000 will be added as -1,-2,-1000
+# pop the min from this heap will return -1000 and multiplying it by -1
+# will give you the max
+maxHeap = []
+heapq.heappush(maxHeap, -8)
+heapq.heappush(maxHeap, -2)
+heapq.heappush(maxHeap, -10)
+
+#max will also be at index 0
+print(-1 * maxHeap[0])
+
+while len(maxHeap):
+    print(-1 * heapq.heappop(maxHeap))
+
+#you can build heap from initial set of concrete values using heapify()
+
+arr = [3,1,4, 8]
+heapq.heapify(arr)
+while arr:
+    print(heapq.heappop(arr))
+
+
+# to create functions, use def keyword and define the functionName and parameters and colon (:)
+# just like conditional statements and loop and body is indented
+def functionName(parameter1, parameter2):        
+    return parameter1 * parameter1 * parameter2 * parameter2
+
+print(functionName(2, 5))
+
+# Nested functions that are inner functions inside of outer functions
+# have access to outer variables in outer function scope by default
+
+def outerFunc(address, phoneNumber):
+    name = "karthik"
+
+    def innerFunc():
+        return name + address + phoneNumber
+    
+    return innerFunc()
+    
+print(outerFunc("N. High St.", "312450"))
+
+
+# You can modify the data within data structures
+# but you can't reassign in nested functions
+#for ex. you can modify colors = ["blue", "red", "green"]
+# but you can't assign colors = ["purple", "orange", "brown"]
+
+def square(arr1, arr2):
+    def helper():
+
+        # this is OK
+        arr1[1] = "yellow"
+
+        # this is NOT OK
+        # arr2 = ["black", "gray", "white"]
+
+        # using nonLocal allow you to reassign and have it be updated
+        # outside of helper() scope
+
+        nonlocal arr2
+        arr2 = ["black", "gray", "white"]
+    helper()
+    print(arr1, arr2)
+
+square(["blue", "red", "green"], ["purple", "orange", "brown"])
+
+
+#Class
+class ColorClass:
+    #Constructor initialized with __init__
+    #self is similar to this keyword in other programming languages
+    #and passed onto every function in Python
+    def __init__(self, paint_brush):
+        #You can use self keyword and dot notation to create member variables
+        self.paint_brush = paint_brush
+        self.length_paint_brush = len(paint_brush)
+
+
+    #Pass in self keyword as a parameter to function
+    # to get access to self's member variables
+    def getPaintBrushType(self):
+        if self.paint_brush == "filbert":
+            return "fan"
+        return "wash"     
+
+    #You can use self keyword to call other functions that belong to self
+    def getPaintBrushBrand(self):
+        if self.getPaintBrushType() == "fan":
+            return "Transon"
+        return "Mont Marte"
+
+colors = ColorClass("filbert")
+print(colors.getPaintBrushBrand())
+
+
+
 
